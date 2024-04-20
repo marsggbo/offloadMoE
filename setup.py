24305b770ba2aa9ccb22c12bc58ad2b49cd77061
marsggbo <1435679023@qq.com>
2024-04-20 06:46:22 +0000

    rename src

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="offMoE",  # 这是包的名称
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple description of the offMoE package",
+    packages=setuptools.find_packages(),  # 自动查找所有包
+    python_requires='>=3.6',
+)
